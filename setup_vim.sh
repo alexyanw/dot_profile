a49bea6e6263ead34bdc3c7505e71c5715ebf338
wenyan <wenyan@qti.qualcomm.com>
2017-02-16 14:48:25 -0800

    fix setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./vimrc ~/.vim/vimrc
+#cp ./vimrc ~/.vim/vimrc
 ln ~/.vim/vimrc ~/.vimrc 
 vim +PluginInstall +qall
