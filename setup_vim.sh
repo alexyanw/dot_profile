531175b4b2b97f641bd01ef568344e9757c02a5c
wenyan <wenyan@qti.qualcomm.com>
2017-02-23 11:29:56 -0800

    adding csh bash rc

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #cp ./vimrc ~/.vim/vimrc
 ln ~/.vim/vimrc ~/.vimrc 
+ln ~/.vim/.cshrc.wenyan ~/.cshrc.wenyan 
+ln ~/.vim/.bashrc.wenyan ~/.bashrc.wenyan 
 vim +PluginInstall +qall
