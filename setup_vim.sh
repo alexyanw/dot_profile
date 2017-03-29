12b2175d19a442cbdc268e22b587ee3d61696d2e
wenyan <wenyan@qti.qualcomm.com>
2017-03-29 10:43:19 -0700

    add tmux.conf

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #cp ./vimrc ~/.vim/vimrc
-ln ~/.vim/vimrc ~/.vimrc 
-ln ~/.vim/.cshrc.wenyan ~/.cshrc.wenyan 
-ln ~/.vim/.bashrc.wenyan ~/.bashrc.wenyan 
+ln -s ~/.vim/vimrc ~/.vimrc 
+ln -s ~/.vim/.cshrc.wenyan ~/.cshrc.wenyan 
+ln -s ~/.vim/.bashrc.wenyan ~/.bashrc.wenyan 
+ln -s ~/.vim/tmux.conf ~/.tmux.conf
 vim +PluginInstall +qall
