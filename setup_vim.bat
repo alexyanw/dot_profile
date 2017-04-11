b1332417fbf922b741e50744c02f760d0b20b622
wenyan <wenyan@qti.qualcomm.com>
2017-04-11 14:25:41 -0700

    update init configs for console

--- setup_vim.bat
+++ setup_vim.bat
@@ -4,17 +4,16 @@ reg import cmd-colors-solarized/solarized-dark.reg
 ::rm -rf cmd-colors-solarized
 
 :: putty
-curl https://raw.githubusercontent.com/altercation/solarized/master/putty-colors-solarized/solarized_dark.reg > putty_solarized_dark.reg
-curl https://raw.githubusercontent.com/altercation/solarized/master/putty-colors-solarized/solarized_light.reg > putty_solarized_light.reg
+reg import putty_solarized_dark.reg
 reg import putty_solarized_light.reg
 
 :: console
 ::curl -O https://raw.githubusercontent.com/nihique/console2-colors-solarized/master/console_solarized_dark.xml
-curl https://raw.githubusercontent.com/stevenharman/console2-solarized/master/console.light.xml > console_solarized_light.xml 
+::curl https://raw.githubusercontent.com/stevenharman/console2-solarized/master/console.light.xml > console_solarized_light.xml 
 
 :: setup vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%/vimfiles/bundle/Vundle.vim
 
 sed "s/~\/\.vim/~\/vimfiles/g" ./vimrc > %USERPROFILE%/_vimrc
 cd %USERPROFILE%
-vim +PluginInstall +qall
+vim +PluginInstall +qal
