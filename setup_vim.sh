81bc90bf4505d325b694fa1ac48378ab364617b3
wenyan <wenyan@qti.qualcomm.com>
2018-03-30 11:44:07 -0700

    update init script

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#cp ./vimrc ~/.vim/vimrc
-ln -s ~/.vim/vimrc ~/.vimrc 
-ln -s ~/.vim/.cshrc.wenyan ~/.cshrc.wenyan 
-ln -s ~/.vim/.bashrc.wenyan ~/.bashrc.wenyan 
-ln -s ~/.vim/tmux.conf ~/.tmux.conf
+SCRIPTDIR=$( dirname "${BASH_SOURCE[0]}" )
+ln -s $SCRIPTDIR/.cshrc.wenyan ~/.cshrc.wenyan 
+ln -s $SCRIPTDIR/.bashrc.wenyan ~/.bashrc.wenyan 
+ln -s $SCRIPTDIR/tmux.conf ~/.tmux.conf
+ln -s $SCRIPTDIR/.gitconfig ~/.gitconfig 
+ln -s $SCRIPTDIR/vimrc ~/.vimrc 
 vim +PluginInstall +qall
